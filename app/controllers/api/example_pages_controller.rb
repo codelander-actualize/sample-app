5afd625c8d3ab42a96406cbfb988f81c5237d684
dani <dani.zaghian@gmail.com>
2019-01-14 12:06:45 -0800

    adds intro to rails code

---
+++ app/controllers/api/example_pages_controller.rb
@@ -0,0 +1,12 @@
+class Api::ExamplePagesController < ApplicationController
+	def hello_method
+		# render web response
+		@message = "Hello!"
+		@time = Time.now.strftime("%I:%M:%S %p")
+		render 'hello.json.jbuilder'
+	end
+
+	def goodbye_method
+		render json: {message: "farewell!"}
+	end
+end
